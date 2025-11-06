4ba48c738d21fd75e3d58e019db620e34ab34377
Ikiiloh <muhriski148@gmail.com>
2025-11-07 06:41:57 +0700

    Update package-lock.json and configure Tailwind CSS integration

--- vite-sempro-invitation/vite.config.js
+++ vite-sempro-invitation/vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss' // <-- 1. Impor plugin Tailwind
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // <-- 2. Tambahkan plugin di sini
+  ],
 })
